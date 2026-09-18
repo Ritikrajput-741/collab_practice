8c3b7c9c0f6cc2bdeafc0daa164165f06ff7342f
RitikRajput <ritik741rajput@gmail.com>
2026-09-19 01:52:33 +0530

    Admin push code

---
+++ script.js
@@ -0,0 +1,12 @@
+const inp = document.getElementById("inp");
+const btn = document.getElementById("btn");
+
+inp.addEventListener;
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const data = inp;
+
+  console.log(data);
+});
